30762e6ff824b073d6d8bdbc010b5745aaa58b69
Anthony <anthonytowry@gmail.com>
2016-05-26 00:20:04 -0500

    HHS Hacking/IT Incident dataset

---
+++ R/HHSHackingIncidents.R
@@ -0,0 +1,13 @@
+#' HHS Hacking Incidents.
+#'
+#' Data frame from United States Department of Health and Human Services with NAs removed
+#' and containing only reports of Hacking/IT Incidents.
+#' 
+#' For the most up-to-date and complete data always visit the HHS Breach Report website.
+#'
+#' @name HHSHackingIncidents
+#'
+#' @format A data frame with 10 variables: \code{Name.of.Covered.Entity}, \code{State},
+#'   \code{Covered.Entity.Type}, \code{Individuals.Affected}, \code{Breach.Submission.Date}, \code{Type.of.Breach}
+#'  \code{Location.of.Breached.Information}, \code{Business.Associate.Present}, \code{Web.Description}, and \code{Date.Submitted}
+NULL
